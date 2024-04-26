57d533984adf8d92753c078a841bb3e9c5433293
Tywon Mosley <tywonmosley@Tywons-MBP.lan>
2024-04-25 21:17:20 -0400

    updated the index.js

--- routes/events.js
+++ routes/events.js
@@ -1,8 +1,9 @@
 const eventsRouter = require("express").Router();
 const jwt = require("jsonwebtoken");
 
+//GET /api/events
 eventsRouter.get('/', async (req, res) => {
-
+res.end();
 })
 
 module.exports = eventsRouter;
--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,14 @@ const jwt = require("jsonwebtoken");
 const eventsRouter = require('./events')
 
 apiRouter.use(async (req, res, next) => {
-
+    //"Bearer eoijnknisdf12332"
+    const prefix = "Bearer "
+    //get Authorization header
+    const auth = req.header("Authorization");  
+    //split "Bearer" and Token 
+    const token = auth.slice(prefix.length);
+    console.log(token)
+    next();
 });
 
 apiRouter.use('/events', eventsRouter)
